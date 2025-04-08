86a4aee18afafd649ec0e23bd94153f3d10dfd4e
irfanpc420 <irfanpc531@gmail.com>
2025-04-08 12:46:30 +0800

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "🦋 ArYAN 🌺";
+const doNotDelete = "𝑷𝑺𝒀𝑪𝑯𝑶 𝑩𝑶𝑻";
 /** 
 * @author NTKhang
 * @author: do not delete it
